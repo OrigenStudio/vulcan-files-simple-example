0309aba971fb2d15a6372cecf7ea899690c7c680
ochicf <oscarchicfarre@gmail.com>
2018-11-13 12:01:48 +0100

    rename create s3 storage provider function

--- packages/example-instagram/lib/modules/pics/fsCollection.js
+++ packages/example-instagram/lib/modules/pics/fsCollection.js
@@ -1,11 +1,11 @@
 import { getSetting } from 'meteor/vulcan:core';
 import { createFSCollection } from 'meteor/origenstudio:vulcan-files';
-import createS3Client from 'meteor/origenstudio:vulcan-files-s3';
+import createS3StorageProvider from 'meteor/origenstudio:vulcan-files-s3';
 
 let storageProvider;
 
 if (getSetting('amazonAWSS3.mainBucket.client')) {
-  storageProvider = createS3Client(
+  storageProvider = createS3StorageProvider(
     getSetting('amazonAWSS3.mainBucket.client'),
     getSetting('amazonAWSS3.mainBucket.cfdomain'),
   );
